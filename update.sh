f09ce312a2bc0d895e5e583c8e71d986b47994c8
Kevin Kellar <kellar.kevin@gmail.com>
2018-09-11 00:11:11 +0000

    edited update so it works with new format

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo "cd /home/kevlar/Awesome-CV/examples" > todo
+echo "cd /home/kevlar/Awesome-CV" > todo
 echo "ls" >> todo
 echo "/usr/local/texlive/2018/bin/x86_64-linux/xelatex resume.tex" >> todo
 echo "exit" >> todo
 cat todo | ssh kevlar@159.65.72.181
 wait
-echo "get /home/kevlar/Awesome-CV/examples/resume.pdf" > todo
+echo "get /home/kevlar/Awesome-CV/resume.pdf" > todo
 cat todo | sftp kevlar@159.65.72.181
 wait
 
