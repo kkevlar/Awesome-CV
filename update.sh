4f89b38692aa32434ece9aa45b509c19615ca503
Kevin Kellar <kellar.kevin@gmail.com>
2018-09-11 21:26:01 +0000

    whole resume tenatively finished for now

--- update.sh
+++ update.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
 echo "cd /home/kevlar/Awesome-CV" > todo
-echo "ls" >> todo
-echo "/usr/local/texlive/2018/bin/x86_64-linux/xelatex resume.tex" >> todo
+echo "bash local.sh" >> todo
 echo "exit" >> todo
-cat todo | ssh kevlar@159.65.72.181
-wait
+echo ""
+cat todo | ssh kevlar@159.65.72.181 | grep ZZZZOK > /dev/null
+if [ $? -eq 1 ]; then
+    bash killrepeat.sh
+    echo "Failure!! Killing repeat updater."
+    exit
+else
+    echo "Typesetting success!"
+fi
+
 echo "get /home/kevlar/Awesome-CV/resume.pdf" > todo
 cat todo | sftp kevlar@159.65.72.181
 wait
 
-echo DONE
+echo "Done. Updated."
+echo ""
 
 
