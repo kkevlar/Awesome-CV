cbc09c15ceecdc6833a1b02e470f385acf967f9f
Kevin Kellar <kellar.kevin@gmail.com>
2018-09-11 21:29:52 +0000

    re fixed update.sh

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ else
 fi
 
 echo "get /home/kevlar/Awesome-CV/resume.pdf" > todo
-cat todo | (sftp kevlar@159.65.72.181 &> /dev/null) &>/dev/null
+cat todo | sftp kevlar@159.65.72.181
 wait
 
 echo "Done. Updated."
